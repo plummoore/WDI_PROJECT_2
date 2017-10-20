3de5ef266a497d4c600e3f51e431ce82b6e3191c
plummoore <plummoore@gmail.com>
2017-10-20 18:54:12 +0100

    user seeds created

--- db/seeds.js
+++ db/seeds.js
@@ -5,7 +5,29 @@ const { dbUri } = require('../config/environment');
 mongoose.connect(dbUri, { useMongoClient: true });
 
 // Require the model
+const User = require('../models/user');
 
 // Drop the model
+User.collection.drop();
 
 // Create the models
+User
+  .create([{
+    firstName: 'Anne',
+    username: 'a',
+    email: 'a@a.com',
+    password: 'password'
+  }, {
+    firstName: 'Belinda',
+    username: 'b',
+    email: 'b@b.com',
+    password: 'password'
+  }, {
+    firstName: 'Carl',
+    username: 'c',
+    email: 'c@c.com',
+    password: 'password'
+  }])
+  .then(users => console.log(`${users.length} users created!`))
+  .catch((err) => console.log(err))
+  .finally(() => mongoose.connection.close());
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
+  firstName: { type: String, required: true, trim: true },
   username: { type: String, required: true, trim: true, unique: true },
   email: { type: String, required: true, trim: true, unique: true },
   password: {type: String, required: true}
