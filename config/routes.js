2fb25058175d9cc333cfb26ce8cd98ce35c4f4f3
plummoore <plummoore@gmail.com>
2017-10-22 11:24:34 +0100

    routes for faces created

--- config/routes.js
+++ config/routes.js
@@ -3,8 +3,11 @@ const router  = express.Router();
 const registration = require('../controllers/registrations');
 const session = require('../controllers/sessions')
 
+
 // A home route
 router.get('/', (req, res) => res.render('homepage'));
+
+//USER SETUP
 
 router.route('/register')
   .get(registration.new)
@@ -18,24 +21,30 @@ router.route('/logout')
   .get(session.delete);
 
 
-
-
 // RESTful routes
 // All URLS should contain the PLURAL... don't chose octopus or people or something silly.
 
-// INDEX - yes
+// FACES SETUP
+
+// INDEX
+router.get('/faces', (req, res) => res.render('faces/index'));
 
 // NEW
+router.get('/faces/new', (req, res) => res.render('faces/new'));
 
 // SHOW
+router.get('/faces/:id', (req, res) => res.render('faces/show'));
 
 // CREATE
+router.post('/faces', (req, res) => res.send('CREATE'));
 
 // EDIT
+router.get('/faces/:id/edit', (req, res) => res.render('faces/edit'));
 
 // UPDATE
+router.put('/faces/:id', (req, res) => res.send('UPDATE'));
 
 // DELETE
-
+router.delete('/faces/:id', (req, res) => res.send('DELETE'));
 
 module.exports = router;
